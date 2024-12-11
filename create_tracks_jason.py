523520786c0c796fc84f6a6dc95d9120985c567a
Julia Breitenbruch <119942939+JBreitenbr@users.noreply.github.com>
2024-12-11 11:33:56 +0000

    created tracksObj.json

--- create_tracks_jason.py
+++ create_tracks_jason.py
@@ -1,27 +1,16 @@
 import pandas as pd
-s=pd.read_csv("batch1.csv")
-"""
-for i in range(len(s)):
-  s.loc[i,"maiuscule"]=s.loc[i,"alphaname"].upper()[0]
+tracksObj={}
+tracksObj["none"]=[{"artist":"none"}]
+df=pd.read_csv("batch1.csv")
+del df["indexOf"]
+artists=df["artist"].unique().tolist()
+for i in range(len(artists)):
+  tracksObj[artists[i]]=[]
+for artist in artists:
+  sub=df[df["artist"]==artist]
+  for j in range(len(sub)):
+    tracksObj[artist].append(sub.iloc[j].to_dict())
 
-mlst=s["maiuscule"].unique().tolist()
-wlst=[]
-for i in range(len(mlst)):
-  wlst.append({})
-for i in range(len(mlst)):
-  wlst[i]["name"]=mlst[i]
-  wlst[i]["bands"]=[]
-blst=[]
-for i in range(len(mlst)):
-  hlp=s[s["maiuscule"]==mlst[i]]["artist"].unique().tolist()
-  blst.append(hlp)
-slst=[[] for i in range(len(mlst))]
-for i in range(len(blst)):
-  for j in range(len(blst[i])):
-    slst[i].append({"name":blst[i][j]})
-for i in range(len(wlst)):
-  wlst[i]["bands"]=slst[i]
-bandObj={}
-bandObj["bandLib"]=wlst
-print(bandObj)
-"""
+tracksLib={}
+tracksLib["tracksLib"]=tracksObj
+print(tracksLib)
