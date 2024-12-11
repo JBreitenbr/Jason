91a8616022fbf4ff5240adc3539c46cceaed198f
Julia Breitenbruch <119942939+JBreitenbr@users.noreply.github.com>
2024-12-11 13:23:23 +0000

    f

--- create_tracks_jason.py
+++ create_tracks_jason.py
@@ -3,14 +3,19 @@ tracksObj={}
 tracksObj["none"]=[{"artist":"none"}]
 df=pd.read_csv("batch1.csv")
 del df["indexOf"]
+del df["album"]
+del df["year"]
+del df["alphaname"]
 artists=df["artist"].unique().tolist()
 for i in range(len(artists)):
   tracksObj[artists[i]]=[]
 for artist in artists:
   sub=df[df["artist"]==artist]
+  
   for j in range(len(sub)):
     tracksObj[artist].append(sub.iloc[j].to_dict())
-
-tracksLib={}
-tracksLib["tracksLib"]=tracksObj
-print(tracksLib)
+import json
+tracks=json.dumps(tracksObj,indent=1)
+#tracksLib={}
+#tracksLib["tracksLib"]=tracksObj
+print(tracks)
